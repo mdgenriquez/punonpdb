d94aff732e359f0291e9f995ed161d3b66388c40
Guadalupe Enriquez <122028844+mdgenriquez@users.noreply.github.com>
2024-12-22 14:44:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import pandas as pd
 import requests
-from rdkit import Chem
-from rdkit.Chem import Draw
-import io
+from chemtools import ChemTools
 
 # Título de la aplicación
-st.title("Visualización de Moléculas en 2D")
+st.title("Visualización de Moléculas en 2D con ChemTools")
 
 # URL del archivo CSV en GitHub
 github_url = "https://raw.githubusercontent.com/mdgenriquez/punonpdb/main/MDG202_cl2.csv"
@@ -28,14 +26,12 @@ try:
         st.subheader("Visualización de Moléculas en 2D")
         for index, row in df.iterrows():
             smiles = row['SMILES']
-            mol = Chem.MolFromSmiles(smiles)
+            mol = ChemTools.from_smiles(smiles)
             if mol:
                 # Generar imagen de la molécula en 2D
-                img = Draw.MolToImage(mol, size=(300, 300))
+                img = mol.to_image(size=(300, 300))  # Genera la imagen en 2D
                 st.image(img, caption=f"Molécula {index + 1}: {smiles}")
             else:
                 st.warning(f"No se pudo procesar el SMILES: {smiles}")
 except Exception as e:
     st.error(f"Error al descargar o procesar el archivo: {e}")
-
-
